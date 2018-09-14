e199e4e3bc0ff71d2f1390aa9165b1286860ffab
Unknown <randomsimpson@gmail.com>
2018-09-14 10:27:51 -0500

    Basic Project Setup and Movement
    Created and created basic layout of game scene, including "Hideout" play area, Player gameobject, and Computer gameobject.
    Created "PlayerMove" script, which allows the player to move, rotate, and detect/interact (via raycast) with the level and objects in the scene.

---
+++ HeroSupport/Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour {
+
+	int moveSpeed = 3;
+	int rotSpeed = 10;
+
+
+	void Start () {
+		
+	}
+	
+
+	void Update () {
+		Move ();
+		Interact();
+	}
+
+
+	void Move () {
+		if (Input.GetAxis("Horizontal") != 0f) {
+			Vector3 playerPos = transform.position;
+			playerPos += Vector3.right * moveSpeed * Input.GetAxis("Horizontal") * Time.deltaTime;
+			transform.position = playerPos;
+
+			Vector3 rotHor = Vector3.RotateTowards(transform.position, (Vector3.right * Input.GetAxis("Horizontal")).normalized, rotSpeed, 0.0f);
+			transform.rotation = Quaternion.LookRotation(rotHor);
+		}
+		if (Input.GetAxis("Vertical") != 0f) {
+			Vector3 playerPos = transform.position;
+			playerPos += Vector3.forward * moveSpeed * Input.GetAxis("Vertical") * Time.deltaTime;
+			transform.position = playerPos;
+
+			Vector3 rotVert = Vector3.RotateTowards(transform.position, (Vector3.forward * Input.GetAxis("Vertical")).normalized, rotSpeed, 0.0f);
+			transform.rotation = Quaternion.LookRotation(rotVert);
+		}
+	}
+
+
+	void Interact () {
+		float reachDist = 0.75f;
+		Ray reachRay = new Ray(transform.position, transform.forward);
+		Debug.DrawRay(transform.position, transform.forward, Color.magenta);
+		RaycastHit reached;
+
+		if (Physics.Raycast(reachRay, out reached, reachDist)) {
+			if (reached.collider.tag == "Construct") {
+				print("Press E to use the " + reached.collider.name);
+
+				if (Input.GetKeyDown(KeyCode.E)) {
+					print("You used the " + reached.collider.name);
+				}
+			}
+		}
+	}
+}
